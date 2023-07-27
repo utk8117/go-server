06f889415255abd790d5c4a68f3f0141f0960c3b
Utkarsha Mhatre <utkarsha.mhatre811@gmail.com>
2023-07-27 14:53:57 +0530

    updated comment

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -9,7 +9,7 @@ import (
 func InitializeRouter(router *gin.Engine) {
 	publicRoutes := router.Group("/")
 	publicRoutes.POST("fetch_details", controller.TrackUsers)               // fetches products of a particular user
-	publicRoutes.POST("add_product_for_user", controller.AddProduct)        // adds products viewed for particular user
+	publicRoutes.POST("add_product_for_user", controller.AddProduct)        // adds products viewed for particular user(tracking action)
 	publicRoutes.POST("check_user", controller.CheckUser)                   // checks for admin user
 	publicRoutes.POST("add_product", controller.NewProduct)                 // adds new product to product table
 	publicRoutes.POST("get_recommendations", controller.GetRecommendations) // get recommendation based on type since user is viewing a product he has all details about the product
